599e57f144446f1f1f434d4f2a5ba88bb89f856f
pruthvi <s.pruthvi22@gmail.com>
2019-11-06 22:59:28 +0000

    Adder function to add string, lists and float

---
+++ add_func.py
@@ -0,0 +1,12 @@
+"""
+Author: Pruthvi Suryadevara
+Email:  pruthvi.suryadevara@tifr.res.in
+Simple code adding strings, lists and float
+
+"""
+def adder(x,y):
+    return(x+y)
+
+print(adder('pink',' floyd'))
+print(adder([1,2,3],[6,7,8]))
+print(adder(2.3,1.8))
