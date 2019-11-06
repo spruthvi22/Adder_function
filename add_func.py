8caf07e9c1de60c97bee8ff3acb6941b3edfe6fd
pruthvi <s.pruthvi22@gmail.com>
2019-11-06 23:27:01 +0000

    Adding arbitary number of arguments (string or floar or list)

--- add_func.py
+++ add_func.py
@@ -1,12 +1,15 @@
 """
 Author: Pruthvi Suryadevara
 Email:  pruthvi.suryadevara@tifr.res.in
-Simple code adding strings, lists and float
+Simple code adding multiple strings, lists and float
 
 """
-def adder(x,y):
-    return(x+y)
+def adder(*arg):
+    su=arg[0]
+    for i in range(len(arg)-1):
+        su=su+arg[i+1]
+    return(su)
 
-print(adder('pink',' floyd'))
+print(adder('pink',' floyd',' rocks'))
 print(adder([1,2,3],[6,7,8]))
 print(adder(2.3,1.8))
