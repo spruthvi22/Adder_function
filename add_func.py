cfa2c8c23d5d7ae974a6a3c6000bbb6c732ae7f8
pruthvi <s.pruthvi22@gmail.com>
2019-11-06 23:51:44 +0000

    keyword arguments

--- add_func.py
+++ add_func.py
@@ -1,15 +1,10 @@
 """
 Author: Pruthvi Suryadevara
 Email:  pruthvi.suryadevara@tifr.res.in
-Simple code adding multiple strings, lists and float
+Simple code adding keyword arguments
 
 """
-def adder(*arg):
-    su=arg[0]
-    for i in range(len(arg)-1):
-        su=su+arg[i+1]
-    return(su)
+def adder(good=1,bad=2,ugly=3):
+    return(good+bad+ugly)
 
-print(adder('pink',' floyd',' rocks'))
-print(adder([1,2,3],[6,7,8]))
-print(adder(2.3,1.8))
+print(adder(ugly=1,good=5))
